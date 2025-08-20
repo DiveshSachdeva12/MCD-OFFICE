1470343f23bf8c0f595b06e5fdf6bb44c6e948f6
DiveshSachdeva12 <Diveshkumarsachdeva85@gmail.com>
2025-08-20 11:22:51 +0530

    all forms and heading update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,14 +4,8 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="header-text">
-        WELCOME TO PANKAJ LUTHRA
-      </div>
-      <img
-        src="/banner.jpeg"
-        alt="Banner"
-        className="banner-img"
-      />
+      <h1 className="header-text">WELCOME TO PANKAJ LUTHRA</h1>
+      <img src="/banner.jpeg" alt="Banner" className="banner-img" />
     </div>
   );
 };
